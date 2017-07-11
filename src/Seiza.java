077d609006e6305c96343270860c90172824774a
Fumiya Takahashi <fumiya.takahashi.sys@gmail.com>
2017-07-11 12:03:02 +0900

    プロジェクトファイル追加

---
+++ src/Seiza.java
@@ -0,0 +1,74 @@
+
+public class Seiza {
+
+
+	public static void main(String[] args){
+		int[] rand = Random.randomInt(12);
+		aries(rand[0] + 1);
+		taurus(rand[1] + 1);
+		gemini(rand[2] + 1);
+		cancer(rand[3] + 1);
+		leo(rand[4] + 1);
+		virgo(rand[5] + 1);
+		libra(rand[6] + 1);
+		scorpio(rand[7] + 1);
+		sagittarius(rand[8] + 1);
+		capricorn(rand[9] + 1);
+		aquarius(rand[10] + 1);
+		pisces(rand[11] + 1);
+
+
+
+	}
+
+
+
+	public static void aries(int num){
+
+	}
+
+	public static void taurus(int num){
+
+	}
+
+	public static void gemini(int num){
+
+	}
+
+	public static void cancer(int num){
+
+	}
+
+	public static void leo(int num){
+
+	}
+
+	public static void virgo(int num){
+
+	}
+
+	public static void libra(int num){
+
+	}
+
+	public static void scorpio(int num){
+
+	}
+
+	public static void sagittarius(int num){
+
+	}
+
+	public static void capricorn(int num){
+
+	}
+
+	public static void aquarius(int num){
+
+	}
+
+	public static void pisces(int num){
+
+	}
+
+}
