15d28c4421c2e262664fe29a59bf66c423eef41f
Ken Muramatsu <muramatsu.ken.sys@gmail.com>
2017-08-22 10:55:11 +0900

    commit new3

--- src/Seiza.java
+++ src/Seiza.java
@@ -25,6 +25,56 @@ public class Seiza {
 
 	public static void aries(int num){
 
+		switch(num){
+		case 1:
+			System.out.println("1位だよ！");
+			System.out.println("人生に影響を及ぼす人との出会いが訪れます");
+			break;
+		case 2:
+			System.out.println("2位だよ！");
+			System.out.println("幸福の女神が微笑む素敵な1日になります");
+			break;
+		case 3:
+			System.out.println("3位だよ！");
+			System.out.println("オープンな態度で過ごすのが大事");
+			break;
+	    case 4:
+	    	System.out.println("4位だよ！");
+	    	System.out.println("暮らしの中に新たなものを取り入れると吉です");
+	    	break;
+	    case 5:
+	    	System.out.println("5位だよ！");
+	    	System.out.println("胸がときめくシーンに遭遇する日になります");
+	    	break;
+	    case 6:
+	    	System.out.println("6位だよ！");
+	    	System.out.println("胸がときめくようなものを見つけられそう");
+	    	break;
+	    case 7:
+	    	System.out.println("7位だよ！");
+	    	System.out.println("イメージチェンジに適している日になります");
+	    	break;
+	    case 8:
+	    	System.out.println("8位だよ！");
+	    	System.out.println("刺激を求めて冒険をしたくなりそう");
+	    	break;
+	    case 9:
+	    	System.out.println("9位だよ！");
+	    	System.out.println("刺激や変化を求める日になりそう");
+	    	break;
+	    case 10:
+	    	System.out.println("10位だよ！");
+	    	System.out.println("無理な約束をしないように注意");
+	    	break;
+	    case 11:
+	    	System.out.println("11位だよ！");
+	    	System.out.println("楽しみを見つけて上手に気分転換を");
+	    	break;
+	    case 12:
+	    	System.out.println("12位だよ！");
+	    	System.out.println("プラス思考でいきましょう");
+	    	break;
+		}
 	}
 
 	public static void taurus(int num){
